e65993ad850404be4f9b2e1feeebb8984ca00a63
p-krikis <p.krikis@prisma2015.onmicrosoft.com>
2023-03-22 16:44:48 +0200

    updated logic | TODO migrate line/bar chart to new format.

--- ReportAppAPI/Models/Module.cs
+++ ReportAppAPI/Models/Module.cs
@@ -5,5 +5,9 @@
         public string Type { get; set; }
         public List<string> Labels { get; set; }
         public List<Dataset> Datasets { get; set; }
+        public int Top { get; set; }
+        public int Left { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
     }
 }
--- ReportAppAPI/Services/ChartService.cs
+++ ReportAppAPI/Services/ChartService.cs
@@ -1,13 +1,17 @@
-﻿using System.Drawing;
-using ScottPlot;
+﻿using ScottPlot;
 using ReportAppAPI.Models;
 using System.Diagnostics;
 using System.Globalization;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.IO.Image;
+using iText.Kernel.Geom;
 
 namespace ReportAppAPI.Services
 {
     public class ChartService
     {
+        private Document _pdfDocument;
         public void PlotChart(Module module)
         {
             try
@@ -19,50 +23,63 @@ namespace ReportAppAPI.Services
                     foreach (var dataset in module.Datasets)
                     {
                         //System.Drawing.Color lineColor = dataset.BorderColor; //ColorTranslator.FromHtml(dataset.BorderColor);
+                        plt.Title("Weather Report Line Chart");
+                        plt.XLabel("Date (DD/MM/YYYY)");
+                        plt.YLabel("Temp/Humidity/Breathability/IAQ");
                         plt.PlotScatter(xAxisData, dataset.Data.ToArray(), markerSize: 5, lineWidth: 1);
                         plt.PlotText(text: dataset.Label, x: xAxisData[0] + 0.3, y: dataset.Data[0] - 0.7, /*color: lineColor,*/ alignment: Alignment.MiddleCenter, fontSize: 10, bold: true);
                         plt.XAxis.TickLabelFormat("dd/MM/yyyy", dateTimeFormat: true);
                         for (int i = 0; i < xAxisData.Length; i++)
                         {
-                            plt.PlotText(text: dataset.Data[i].ToString(), x: xAxisData[i], y: dataset.Data[i] + 0.4, /*color: lineColor,*/ alignment: Alignment.MiddleCenter, fontSize: 10, bold: true);
+                            plt.PlotText(text: dataset.Data[i].ToString(), x: xAxisData[i], y: dataset.Data[i] + 0.4, alignment: Alignment.MiddleCenter, fontSize: 10, bold: true, color: System.Drawing.Color.Black);
                         }
                     }
-                    plt.SaveFig("C:\\Users\\praktiki1\\Desktop\\APIdump\\PNGs\\line_chart.png");
                 }
                 else if (module.Type == "bar")
                 {
-                    double[] xAxisData = GenerateXAxisData(module.Labels.Count);
+                    double[] xAxisData = GenerateXAxisData(module.Labels);
                     foreach (var dataset in module.Datasets)
                     {
-                        plt.PlotBar(xAxisData, dataset.Data.ToArray());
+                        plt.PlotBar(xAxisData, dataset.Data.ToArray(), barWidth: 0.5, showValues: true);
                         plt.XAxis.TickLabelFormat("dd/MM/yyyy", dateTimeFormat: true);
                     }
-                    plt.SaveFig("C:\\Users\\praktiki1\\Desktop\\APIdump\\PNGs\\bar_chart.png");
                 }
                 else if (module.Type == "pie")
                 {
                     double[] values = module.Datasets[0].Data.ToArray();
                     string[] labels = module.Labels.ToArray();
-                    plt.PlotPie(values, labels);
-                    plt.SaveFig("C:\\Users\\praktiki1\\Desktop\\APIdump\\PNGs\\pie_chart.png");
+                    var pie = plt.AddPie(values);
+                    pie.Explode = true;
+                    pie.ShowValues = true;
+                    pie.SliceLabels = labels;
+                    plt.Legend();
                 }
+                else if (module.Type == "polarArea") // currently no use, coxcomb_chart
+                {
+                    double[] values = module.Datasets[0].Data.ToArray();
+                    string[] labels = module.Labels.ToArray();
+                    var polarArea = plt.AddCoxcomb(values);
+                    polarArea.FillColors = plt.Palette.GetColors(5, 0, 0.5);
+                    polarArea.SliceLabels = labels;
+                }
+                //else if (module.Type == "scatter") // currently no use
+                //{
+
+                //}
                 else
                 {
                     Debug.WriteLine($"Unsupported Chart type: {module.Type}");
                 }
+                plt.SaveFig($"C:\\Users\\praktiki1\\Desktop\\APIdump\\PNGs\\{module.Type}_chart.png");
             }
             catch (Exception ex)
             {
                 Debug.WriteLine($"Error plotting chart: {ex.Message}");
             }
         }
-        private double[] GenerateXAxisData(int count)
+        private double[] GenerateXAxisData(List<string> labels)
         {
-            double[] xAxisData = new double[count];
-            for (int i= 0; i < count; i++)
-            {
-                xAxisData[i] = i + 1;
-            }
+            double[] xAxisData = labels.Select(dateString => DateTime.ParseExact(dateString, "MM/dd/yyyy", CultureInfo.InvariantCulture).ToOADate()).ToArray();
             return xAxisData;
         }
     }
