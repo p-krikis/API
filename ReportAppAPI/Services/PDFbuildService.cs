93aa100c1c352e403c9dcf72de183c7364d21d37
p-krikis <p.krikis@prisma2015.onmicrosoft.com>
2023-03-31 14:48:40 +0300

    fixed alignment issues

--- ReportAppAPI/Services/PDFbuildService.cs
+++ ReportAppAPI/Services/PDFbuildService.cs
@@ -1,4 +1,5 @@
 ﻿using iText.IO.Image;
+using iText.Kernel.Geom;
 using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Layout.Element;
@@ -47,6 +48,7 @@ namespace ReportAppAPI.Services
             }
             document.Add(pdfTable);
         }
+
         private void CreatePanelTable(Module module, Document document)
         {
             int numColumns = 1;
@@ -55,15 +57,19 @@ namespace ReportAppAPI.Services
             panelTable.AddCell(new Cell().Add(new Paragraph("The ").Add(module.Aggregate).Add(" value of ").Add(module.Datasets[0].Label).Add(" from ").Add(module.From).Add(" to ").Add(module.To).Add(" was ").Add(module.Datasets[0].Data[0].ToString())).SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER));
             document.Add(panelTable);
         }
+
         public void buildPdf(List<Module> modules)
         {
             string pngFolderPath = @"C:\Users\praktiki1\Desktop\APIdump\PNGs"; //image sauce
             string pdfPath = @"C:\Users\praktiki1\Desktop\APIdump\PDFs\report.pdf"; //pdf dump loc
+            float pageWidth = 776f; //hardcoded page width 2x images (2x368px) + 2x padding (2x20px) left-right
             using (FileStream stream = new FileStream(pdfPath, FileMode.Create, FileAccess.Write))
             {
                 PdfWriter writer = new PdfWriter(stream);
                 PdfDocument pdfDocument = new PdfDocument(writer);
+                pdfDocument.SetDefaultPageSize(new PageSize(pageWidth, PageSize.A4.GetHeight())); //hardcoded custom page size
                 Document document = new Document(pdfDocument);
+                List<Tuple<ImageData, float, float>> images = new List<Tuple<ImageData, float, float>>();
                 foreach (var module in modules)
                 {
                     if (module.Type == "table")
@@ -75,21 +81,20 @@ namespace ReportAppAPI.Services
                     {
                         CreatePanelTable(module, document);
                     }
-                }
-                DirectoryInfo directoryInfo = new DirectoryInfo(pngFolderPath);
-                FileInfo[] graphImages = directoryInfo.GetFiles("*.png");
-                int imageCounter = 0;
-                foreach (FileInfo graphImage in graphImages)
-                {
-                    ImageData imageData = ImageDataFactory.Create(graphImage.FullName);
-                    Image pdfImage = new Image(imageData);
-                    pdfImage.SetAutoScale(true);
-                    document.Add(pdfImage);
-                    imageCounter++;
-                    if (imageCounter % 2 == 0) //2 images per page
+                    else
                     {
-                        document.Add(new AreaBreak());
+                        float x = module.Left;
+                        float y = module.Top;
+                        string imagePath = System.IO.Path.Combine(pngFolderPath, $"{module.Aggregate}_{module.Type}_chart.png");
+                        ImageData imageData = ImageDataFactory.Create(imagePath);
+                        images.Add(new Tuple<ImageData, float, float>(imageData, x, y));
                     }
+                }
+                foreach (var image in images)
+                {
+                    Image pdfImage = new Image(image.Item1);
+                    pdfImage.SetFixedPosition(image.Item2, pdfDocument.GetPage(1).GetPageSize().GetHeight() - image.Item3 - pdfImage.GetImageHeight());
+                    document.Add(pdfImage);
                 }
                 document.Close();
             }
